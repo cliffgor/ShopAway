99f38157063b5ba7cef0abfbf3e34b177e3b3fc7
cliffgor <clierozigy@gmail.com>
2017-10-24 12:17:35 +0300

    added a list view on the acivity_main.xml

--- app/src/main/java/com/example/cliff/shopaway/MainActivity.java
+++ app/src/main/java/com/example/cliff/shopaway/MainActivity.java
@@ -6,19 +6,30 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> shoppingList = null;
+    ArrayAdapter<String> adapter=null;
+    ListView lv =null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> shoppingList = null;
-        ArrayAdapter<String> adapter=null;
-        ListView lv =null;
 
         shoppingList = new ArrayList<>();
+        Collections.addAll(shoppingList, "eggs", "tommatoes", "apples", "milk");
+        shoppingList.addAll(Arrays.asList("dog food", "chapstiks", "bread", "Napkins"));
+        shoppingList.add("sunscreen");
+        shoppingList.add("toothpaste");
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, shoppingList);
+        lv = (ListView) findViewById(R.id.listview);
+        lv.setAdapter(adapter);
+
 
 
 
