d89c86ccba79e80b24a152278951fd612ac76b1a
cliffgor <clierozigy@gmail.com>
2017-10-25 13:16:16 +0300

    Modifications made; *added an add icon; *added an onClick method that enables one to add an item an it converts a string toUperCase from toLowerCase

--- app/src/main/java/com/example/cliff/shopaway/MainActivity.java
+++ app/src/main/java/com/example/cliff/shopaway/MainActivity.java
@@ -10,6 +10,14 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import android.app.AlertDialog;
+import android.widget.EditText;
+import android.content.DialogInterface;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> shoppingList = null;
@@ -56,6 +64,38 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+
+        if (id == R.id.action_add) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Add Item");
+            final EditText input = new EditText(this);
+            builder.setView(input);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    shoppingList.add(preferredCase(input.getText().toString()));
+                    Collections.sort(shoppingList);
+                    lv.setAdapter(adapter);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public static String preferredCase(String original)
+    {
+        if (original.isEmpty())
+            return original;
+
+        return original.substring(0, 1).toUpperCase() + original.substring(1).toLowerCase();
     }
 }
